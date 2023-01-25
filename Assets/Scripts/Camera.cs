b6c35aaf398bbcc5512b76c3445c01612ce4295d
Ofijo79 <oriolfijo2004@gmail.com>
2023-01-25 11:02:11 +0100

    Jump and run

--- Assets/Scripts/Camera.cs
+++ Assets/Scripts/Camera.cs
@@ -6,6 +6,11 @@ public class Camera : MonoBehaviour
 {
     private Transform target;
     public Vector3 offset;
+    // Vectores para limitar el movimiento de la camara en Y y X
+    public Vector2 limitX;
+    public Vector2 limitY;
+
+    public float interpolationRatio; 
 
     // Start is called before the first frame update
     void Start()
@@ -14,8 +19,22 @@ public class Camera : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        transform.position = target.position + offset;
+        // posicion deseada de la camara
+        Vector3 desiredPosition = target.position + offset;
+
+        // limitamos en la posicion x
+        float clampX = Mathf.Clamp(desiredPosition.x, limitX.x, limitX.y);
+
+        // limitamos en la posicion y
+        float clampY = Mathf.Clamp(desiredPosition.y, limitY.x, limitY.y);
+
+        // posicion limitada en X e Y
+        Vector3 clampedPosition = new Vector3(clampX, clampY, desiredPosition.z);
+
+        Vector3 lerpedPosition = Vector3.Lerp(transform.position, clampedPosition, interpolationRatio);
+        
+        transform.position = lerpedPosition;
     }
 }
--- Assets/Scripts/MarioMovement.cs
+++ Assets/Scripts/MarioMovement.cs
@@ -12,6 +12,7 @@ public class MarioMovement : MonoBehaviour
     private Rigidbody2D rBody;
     public float jumpForce = 3f;
     private GroundSensor sensor;
+    public Animator anim;
 
     // Start is called before the first frame update
     void Start()
@@ -19,9 +20,11 @@ public class MarioMovement : MonoBehaviour
         sensor = GameObject.Find("GroundSensor").GetComponent<GroundSensor>();
         rBody = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
-        
+        anim = GetComponent<Animator>();
+
         playerHealth = 10;
         Debug.Log(texto);  
+        
     }
 
     // Update is called once per frame
@@ -34,15 +37,22 @@ public class MarioMovement : MonoBehaviour
         if(horizontal < 0)
         {
             spriteRenderer.flipX = true;
+            anim.SetBool("IsRunning", true);
         }
         else if(horizontal > 0)
         {
             spriteRenderer.flipX = false;
+            anim.SetBool("IsRunning", true);
+        }
+        else
+        {
+            anim.SetBool("IsRunning", false);
         }
 
         if(Input.GetButtonDown("Jump") && sensor.isGrounded)
         {
             rBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            anim.SetBool("IsJumping", true);
         }
     }
 }
