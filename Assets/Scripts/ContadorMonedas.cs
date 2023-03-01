0c2c30949908b9c21ba9049a7c2813c4bd1b904b
Ofijo79 <oriolfijo2004@gmail.com>
2023-03-01 11:04:13 +0100

    Score

---
+++ Assets/Scripts/ContadorMonedas.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ContadorMonedas : MonoBehaviour
+{
+    private int score;
+    public Text scoreText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision) 
+    {
+        if(collision.gameObject.tag == "Coin")
+        {
+            score++;
+            scoreText.text = "score" + score;
+        }
+    }
+}
